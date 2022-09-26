37994f6c871b5fb92106e7c1e87be36f87cbb50e
Adam Ogiba <adamek.og@gmail.com>
2022-09-26 18:55:47 +0200

    Try auto build examples

---
+++ example/build_all.sh
@@ -0,0 +1,14 @@
+CXX="${CXX:-g++}" 
+CXXFLAGS="${CXXFLAGS:--std=c++11}"
+
+$CXX $CXXFLAGS generic_trait_with_output_2.cpp && ./a.out
+$CXX $CXXFLAGS generic_trait_with_output.cpp && ./a.out
+$CXX $CXXFLAGS generic_trait.cpp && ./a.out
+$CXX $CXXFLAGS json_serialize.cpp && ./a.out
+$CXX $CXXFLAGS macro.cpp && ./a.out
+$CXX $CXXFLAGS shared_ptr.cpp && ./a.out
+$CXX $CXXFLAGS simple_print_generic.cpp && ./a.out
+$CXX $CXXFLAGS simple_print.cpp && ./a.out
+$CXX $CXXFLAGS simplest_print.cpp && ./a.out
+$CXX $CXXFLAGS simple_print_generic.cpp && ./a.out
+$CXX $CXXFLAGS trait_in_vector.cpp && ./a.out
